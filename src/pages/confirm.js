3e8f42337d764b1c383f8130fad369effeaf461f
Theek-skate <atheek990@gmail.com>
2024-10-03 07:58:25 +0530

    use State on pick up and drop off
    adding use state and saving these locations

--- src/pages/confirm.js
+++ src/pages/confirm.js
@@ -1,48 +1,54 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import tw from "tailwind-styled-components";
 import Link from "next/link";
 import Map from "./components/Map";
 
 const Confirm = () => {
+  const [pickupCoordinates, setPickupCoordinates] = useState();
+  const [dropoffCoordinates, setDropoffCoordinates] = useState();
 
   const getPickupCoordinates = () => {
-    const pickup  = "Borella";
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
-      new URLSearchParams({
-         access_token: "pk.eyJ1IjoidGhlZWtza2F0ZSIsImEiOiJjbTFkNGUyNDcyamZyMm1wemZ2dmh3NHhvIn0.iO_Lg1G4FdNJE7mAbyUp-w",
-         limit:1
-      })
+    const pickup = "Borella";
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
+        new URLSearchParams({
+          access_token:
+            "pk.eyJ1IjoidGhlZWtza2F0ZSIsImEiOiJjbTFkNGUyNDcyamZyMm1wemZ2dmh3NHhvIn0.iO_Lg1G4FdNJE7mAbyUp-w",
+          limit: 1,
+        })
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log("pickup")
-        console.log(data.features[0].center);
+        setPickupCoordinates(data.features[0].center);
       });
   };
 
   const getDropoffCoordinates = () => {
     const dropoff = "Slave Island";
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` +
-      new URLSearchParams({
-         access_token: "pk.eyJ1IjoidGhlZWtza2F0ZSIsImEiOiJjbTFkNGUyNDcyamZyMm1wemZ2dmh3NHhvIn0.iO_Lg1G4FdNJE7mAbyUp-w",
-         limit:1
-      })
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` +
+        new URLSearchParams({
+          access_token:
+            "pk.eyJ1IjoidGhlZWtza2F0ZSIsImEiOiJjbTFkNGUyNDcyamZyMm1wemZ2dmh3NHhvIn0.iO_Lg1G4FdNJE7mAbyUp-w",
+          limit: 1,
+        })
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log('dropoff')
-        console.log(data.features[0].center);
+       
+        setDropoffCoordinates(data.features[0].center);
       });
-  }
+  };
 
   // testing main branch
   useEffect(() => {
-   
-    getPickupCoordinates()
-    getDropoffCoordinates()
-  },[]) 
+    getPickupCoordinates();
+    getDropoffCoordinates();
+  }, []);
 
-  
+
+  console.log(pickupCoordinates);
+  console.log(dropoffCoordinates);
 
   return (
     <Wrapper>
@@ -55,7 +61,10 @@ const Confirm = () => {
       <RiderContainer>
         {/* Rider Selector */}
 
+
         {/* Confirm Button */}
+
+      
       </RiderContainer>
     </Wrapper>
   );
@@ -64,9 +73,6 @@ const Confirm = () => {
 export default Confirm;
 
 const Wrapper = tw.div`
-    flex flex-col flex-1 h-screen bg-white
-`;
-
+    flex flex-col flex-1 h-screen bg-white`;
 const RiderContainer = tw.div`
-    flex flex-1
-`;
+    flex flex-1 text-black  `;
